b4043454fef98837895bf16f7145631e5832afeb
jordi lopez galera <jordi8284@gmail.com>
2020-05-10 10:33:40 +0200

    add autoupdater

--- electron/main.ts
+++ electron/main.ts
@@ -1,8 +1,26 @@
-import { app, BrowserWindow , autoUpdater } from 'electron';
+import { app, BrowserWindow, autoUpdater, dialog } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
 let win: BrowserWindow;
+
+const server = 'https://daily-standup-timer-updater-hc0v9umil.now.sh/';
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  };
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) { autoUpdater.quitAndInstall(); }
+  });
+});
+autoUpdater.setFeedURL({ url: feed });
 
 app.on('ready', createWindow);
 
